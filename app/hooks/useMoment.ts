d3f020a9c21bcd7d6ff9cc35c3b86a91d1abdbd3
hiro1125 <hiro112538@gmail.com>
2023-10-30 18:15:59 +0900

    fix: 更新するたびに、レンダリングが走るための不具合修正

--- app/hooks/useMoment.ts
+++ app/hooks/useMoment.ts
@@ -2,19 +2,19 @@
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 
-export const useMoment = () => {
-  moment.updateLocale('ja', {
-    weekdays: [
-      '日曜日',
-      '月曜日',
-      '火曜日',
-      '水曜日',
-      '木曜日',
-      '金曜日',
-      '土曜日',
-    ],
-  });
+moment.updateLocale('ja', {
+  weekdays: [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日',
+  ],
+});
 
+export const useMoment = () => {
   const [dateTime, setDateTime] = useState(
     moment().format('YYYY[年]MM[月]DD[日] (dddd) HH時mm分ss秒')
   );
