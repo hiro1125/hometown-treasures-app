d734b24fb1bc4c44b13175783105247a2ec51718
hiro1125 <hiro112538@gmail.com>
2023-10-31 12:56:36 +0900

    feat: 検索アイコンと追加のアイコンを追加

--- app/components/molecules/MolHeader.tsx
+++ app/components/molecules/MolHeader.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { useDateTime } from '@/app/hooks/useDateTime';
 import React from 'react';
+import { useDateTime } from '@/app/hooks/useDateTime';
+import { IoMdSearch, IoIosAddCircleOutline } from 'react-icons/io';
 
 const MolHeader = () => {
   const dateTime = useDateTime();
@@ -8,6 +9,12 @@ const MolHeader = () => {
     <div className='bg-red-100 h-20 flex items-center justify-center gap-10'>
       <div className='flex relative items-center'>
         <div className='text-center text-4xl'>{dateTime}</div>
+      </div>
+      <div className='left-2 flex items-center justify-center'>
+        <IoMdSearch size={50} />
+      </div>
+      <div className='left-2 flex items-center justify-center'>
+        <IoIosAddCircleOutline size={50} />
       </div>
     </div>
   );
