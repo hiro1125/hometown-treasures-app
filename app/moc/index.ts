848cd2ba628912596192054eb5c6aac236cc3b20
hiro1125 <hiro112538@gmail.com>
2023-10-31 14:50:07 +0900

    feat: mocデータの作成及び型を付与

---
+++ app/moc/index.ts
@@ -0,0 +1,29 @@
+import { ApiListData } from '@/app/types';
+
+export const listData: ApiListData[] = [
+  {
+    orderDate: '2023年4月27日',
+    productName: '松阪牛すき焼き（モモ・バラ・カタ）400g',
+    address: '三重県 大台町',
+    amountOfMoney: 10000,
+  },
+  {
+    orderDate: '2023年5月30日',
+    productName: 'かつお タタキ 2.1kg',
+    address: '高知県 須崎市',
+    amountOfMoney: 10000,
+  },
+  {
+    orderDate: '2023年4月27日',
+    productName: 'さがびより 12kg（5kg×2袋,2kg×1袋）',
+    address: '佐賀県 吉野ヶ里町',
+    amountOfMoney: 10000,
+  },
+  {
+    orderDate: '2023年4月27日',
+    productName:
+      'エリエール i:na（イーナ）トイレットティシュー 12Rシングル（100m巻）（12ロール×6パック）',
+    address: '岐阜県 可児市',
+    amountOfMoney: 18000,
+  },
+];
---
+++ app/types/index.ts
@@ -0,0 +1,6 @@
+export type ApiListData = {
+  orderDate: string;
+  productName: string;
+  address: string;
+  amountOfMoney: number;
+};
