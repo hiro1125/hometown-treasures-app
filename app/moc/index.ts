29775ca2bdfb80a13c6e7679850686f258306bb2
hiro1125 <hiro112538@gmail.com>
2023-10-31 16:19:24 +0900

    fix: mocデータにおける日付の修正

--- app/moc/index.ts
+++ app/moc/index.ts
@@ -2,25 +2,25 @@ import { ApiListData } from '@/app/types';
 
 export const listData: ApiListData[] = [
   {
-    orderDate: '2023年4月27日',
+    orderDate: '2023年4月29日',
     productName: '松阪牛すき焼き（モモ・バラ・カタ）400g',
     address: '三重県 大台町',
     amountOfMoney: 10000,
   },
   {
-    orderDate: '2023年5月30日',
+    orderDate: '2023年5月21日',
     productName: 'かつお タタキ 2.1kg',
     address: '高知県 須崎市',
     amountOfMoney: 10000,
   },
   {
-    orderDate: '2023年4月27日',
+    orderDate: '2023年6月10日',
     productName: 'さがびより 12kg（5kg×2袋,2kg×1袋）',
     address: '佐賀県 吉野ヶ里町',
     amountOfMoney: 10000,
   },
   {
-    orderDate: '2023年4月27日',
+    orderDate: '2023年7月23日',
     productName:
       'エリエール i:na（イーナ）トイレットティシュー 12Rシングル（100m巻）（12ロール×6パック）',
     address: '岐阜県 可児市',
