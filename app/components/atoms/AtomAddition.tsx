5eec95adfa60626ba1f5118f964017686907dd45
hiro1125 <hiro112538@gmail.com>
2023-11-03 10:50:25 +0900

    refactor: 不要な記述の削除

--- app/components/atoms/AtomAddition.tsx
+++ app/components/atoms/AtomAddition.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import { IoIosAddCircleOutline } from 'react-icons/io';
 
 const AtomAddition = () => (
-  <>
-    <div className='left-2 flex items-center justify-center cursor-pointer'>
-      <Link href={'register'} target='blank'>
-        <IoIosAddCircleOutline size={50} />
-      </Link>
-    </div>
-  </>
+  <div className='left-2 flex items-center justify-center cursor-pointer'>
+    <Link href={'register'} target='blank'>
+      <IoIosAddCircleOutline size={50} />
+    </Link>
+  </div>
 );
 
 export default AtomAddition;
