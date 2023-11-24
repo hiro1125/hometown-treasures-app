67acd2e36d6573311e89a0b457a6918313f2edc3
hiro1125 <hiro112538@gmail.com>
2023-11-24 22:46:28 +0900

    fix: リロードすると、初期値の金額が表示される不具合の修正
    これに伴い、ローディングアイコンを追加しました。

--- app/components/molecules/MolTotalAmount.tsx
+++ app/components/molecules/MolTotalAmount.tsx
@@ -1,33 +1,51 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Typography from '@mui/material/Typography';
-import { Card, CardContent } from '@mui/material';
+import { Box, Card, CardContent, CircularProgress } from '@mui/material';
 import { TYPOGRAPHY_TEXT, totalAmountValue } from '@/app/contents';
 import { listData } from '@/app/moc';
 import { useDateTime } from '@/app/hooks/useDateTime';
 
 const MolTotalAmount = () => {
   const [count, setCount] = useState(totalAmountValue);
+  const [isAmountLoading, setIsAmountLoading] = useState(false);
   const getYear = useDateTime('YYYY');
 
-  useEffect(() => {
-    const presentAmount = listData
-      .filter(
-        (item) => new Date(item.orderDate).getFullYear().toString() === getYear
-      )
-      .reduce((total, item) => total + item.amountOfMoney, 0);
+  const presentAmount = useMemo(
+    () =>
+      listData
+        .filter(
+          (item) =>
+            new Date(item.orderDate).getFullYear().toString() === getYear
+        )
+        .reduce((total, item) => total + item.amountOfMoney, 0),
+    [getYear]
+  );
 
-    setCount(totalAmountValue - presentAmount);
-  }, [getYear]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(totalAmountValue - presentAmount);
+      setIsAmountLoading(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [presentAmount]);
 
   const formattedDonationAmount = count.toLocaleString('ja-JP');
 
   return (
     <Card sx={{ minWidth: 275 }} className={'m-10'}>
       <CardContent className={'bg-gray-100 flex items-center justify-center'}>
-        <Typography className={'text-4xl'}>
-          {`${TYPOGRAPHY_TEXT.BALANCE_OF_DONATIONS} ${formattedDonationAmount} 円`}
-        </Typography>
+        {isAmountLoading ? (
+          <Typography className={'text-4xl'}>
+            {`${TYPOGRAPHY_TEXT.BALANCE_OF_DONATIONS} ${formattedDonationAmount} 円`}
+          </Typography>
+        ) : (
+          <Box className=' text-center'>
+            <CircularProgress />
+            <Typography>読み込みしています...</Typography>
+          </Box>
+        )}
       </CardContent>
     </Card>
   );
