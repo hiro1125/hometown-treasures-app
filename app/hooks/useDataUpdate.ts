208d64f3e6c87d43f43030389a85b7fa94c7915e
hiro1125 <hiro112538@gmail.com>
2023-11-30 21:34:48 +0900

    fix: Vercelのデプロイに失敗した、Type errorを修正しました。
    仮に一旦anyとします。

--- app/hooks/useDataUpdate.ts
+++ app/hooks/useDataUpdate.ts
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import { ApiListData } from '@/app/types';
 import { listData as initialListData } from '@/app/moc';
 
 export const useDataUpdate = () => {
   const [listData, setListData] = useState(initialListData);
 
-  const handleAddData = (newData: ApiListData) => {
+  const handleAddData = (newData: any) => {
     setListData((prevData) => [...prevData, newData]);
   };
 
